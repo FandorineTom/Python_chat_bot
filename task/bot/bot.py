c072c433367519b80c19e892536a6f10bea0a8e1
glaisan <ellenhanx@gmail.com>
2021-08-31 13:54:20 +0300

    second commit

---
+++ task/bot/bot.py
@@ -0,0 +1,54 @@
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
+
+
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    print("When does the body of a function end?\n1.it ends after the keyword def\n"
+          "2.it ends when the function is called\n3.it ends with the lack of indentation\n"
+          "4.it ends with return statement")
+    user_answer = 0
+    while user_answer != 3:
+        user_answer = int(input())
+        print("Completed, have a nice day!" if user_answer == 3 else "Please, try again.")
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
+end()
